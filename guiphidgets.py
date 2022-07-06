b79808c42dfe71a2203b82ed3947bde3860bd945
Jigar Patel <pateljj04@gmail.com>
2022-07-06 13:10:41 -0400

    update

--- guiphidgets.py
+++ guiphidgets.py
@@ -18,12 +18,16 @@ temperatureSensor.openWaitForAttachment(1000)
 def getTemp():
     #Set Temperature Variable
     showTemp = str(temperatureSensor.getTemperature()) + " °C"
+    
+    # Print on GUI 
+    message.value = showTemp
     # Print for Debugging in console comment later
     print(" Temperature: " + showTemp )
 
+
 # When button is pressed get temperature    
 btn.when_pressed = getTemp
-
+message = Text(app)
 button = PushButton(app, text="Get Temperature", command=getTemp)
 
 app.display()
