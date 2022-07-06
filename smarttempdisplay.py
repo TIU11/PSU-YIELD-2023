5077c8d081ef4534a54b5638bde9b13dd5f27709
Jigar Patel <pateljj04@gmail.com>
2022-07-06 13:00:44 -0400

    autorefresh

--- smarttempdisplay.py
+++ smarttempdisplay.py
@@ -14,20 +14,22 @@ lcd = LCD()
 temperatureSensor.openWaitForAttachment(1000)
 lcd.openWaitForAttachment(1000)
 
-#Set Temperature Variable
-showTemp = str(temperatureSensor.getTemperature()) + " °C"
+# Set the LCD to update automatically
+lcd.setAutoFlush(True)
+
 
 def getTemp():
+    #Set Temperature Variable
+    showTemp = str(temperatureSensor.getTemperature()) + " °C"
     lcd.writeText(LCDFont.FONT_6x12, 0, 0, showTemp)
     # Print for Debugging in console comment later
     print(" Temperature: " + str(temperatureSensor.getTemperature()) + " °C" )
     
 
-
 #Use your Phidgets 
 while(True):
     getTemp()
     time.sleep(1)
-    
+    lcd.flush()
 
   
