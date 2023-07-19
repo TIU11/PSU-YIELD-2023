83d45bb386f3dc5df12d28936695f58acc5a7337
Jigar Patel <pateljj04@gmail.com>
2023-07-19 10:26:31 -0400

    Update logmultipledata.py

--- logmultipledata.py
+++ logmultipledata.py
@@ -2,15 +2,18 @@
 #Add Phidgets library
 from Phidget22.Phidget import *
 from Phidget22.Devices.TemperatureSensor import *
+from Phidget22.Devices.LightSensor import *
+
 #Required for sleep statement
 from time import time, sleep, strftime
 import csv
 
 #Create
 temperatureSensor = TemperatureSensor()
-
+lightSensor = LightSensor()
 #Open
 temperatureSensor.openWaitForAttachment(1000)
+lightSensor.openWaitForAttachment(1000)
 
 #Record data points
 count = 0
@@ -24,8 +27,9 @@ with open ('phidgets_temperature.csv','w') as datafile:
         now = time()
         now = strftime('%I:%M:%S %p') # 2:05:01 PM
         temp = temperatureSensor.getTemperature()
-        light = lightSensor.getLuminance()
+        light = lightSensor.getIlluminance()
         return [now, temp, light]
+    
     #Use your Phidgets
     
     while (True):
@@ -43,5 +47,5 @@ with open ('phidgets_temperature.csv','w') as datafile:
             print("Logging complete, exiting program")
             exit()
         
-        time.sleep(10)
+        sleep(10)
   
